c8a3b4c8f97d808a20468137a4d74cafcd0304c7
TulakHordia <tulak@hord.dev>
2024-03-31 11:50:33 +0300

    xdd

---
+++ README.md
@@ -0,0 +1,11 @@
+# World of Games: The Epic Journey
+
+Welcome to the World of Games: The Epic Journey! This Python program offers you an exciting collection of three games to enjoy. Embark on this epic journey filled with fun and challenges!
+
+## Features
+
+- **Memory Game:** Test your memory skills as you recall a sequence of numbers that appear for a brief moment.
+  
+- **Guess Game:** Put your guessing abilities to the test as you try to predict a number chosen by the computer.
+  
+- **Currency Roulette:** Take a gamble and try to guess the value of a random amount of USD in ILS (Israeli Shekels).
--- app.py
+++ app.py
@@ -4,26 +4,30 @@ def welcome():
 
 
 def start_play():
-    choice_prompt = "Please choose a game to play: "
-    mem_game = "1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back."
-    guess_game = "2. Guess Game - guess a number and see if you chose like the computer."
-    currency_game = "3. Currency Roulette - try and guess the value of a random amount of USD in ILS."
-    games_list = [choice_prompt, mem_game, guess_game, currency_game]
-    game_names = ['Memory Game', 'Guess Game', 'Currency Roulette']
+    games = {
+        1: "Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back.",
+        2: "Guess Game - guess a number and see if you chose like the computer.",
+        3: "Currency Roulette - try and guess the value of a random amount of USD in ILS."
+    }
 
-    for x in games_list:
-        print(x)
+    print("Please choose a game to play:")
+    for key, value in games.items():
+        print(f"{key}. {value}")
+
     while True:
         choice = int(input("Select your game (1-3): "))
-        if choice in range(1, 4):
+        if choice in games:
             while True:
                 difficulty_choice = int(input("Choose a difficulty (1-5): "))
-                if difficulty_choice in range(1, 6):
+                if 1 <= difficulty_choice <= 5:
                     print(f"Your difficulty is {difficulty_choice}")
-                    break
+                    print(f"Your game is {games[choice]}")
+                    return
                 else:
                     print("Invalid input, Difficulty must be between 1 and 5")
-            print(f"Your game is {game_names[choice - 1]}")
-            break
         else:
             print("Invalid input, Please select a number between 1 and 3")
+
+
+welcome()
+start_play()
